aa346443d6bf598feb1940bbf23c9baf5eef6cc3
Lean 4 VS Code Extension <>
2026-08-12 08:37:43 +0700

    Initial commit

---
+++ lean/Term/Main.lean
@@ -0,0 +1,4 @@
+import Term
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ lean/Term/Term.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `Term` library.
+-- Import modules here that should be built as part of the library.
+import Term.Basic
